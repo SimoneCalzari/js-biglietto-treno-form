a11b4ee9260a6c6b0bc8a1b42489dddad133bd06
SimoneCalzari <simone.calzari@outlook.it>
2023-11-07 22:50:00 +0100

    feat: milestone 2

--- js/main.js
+++ js/main.js
@@ -1,44 +1,61 @@
 'use strict';
 
+// elementi del dom necessari per il problema
 const nomeCognome = document.getElementById('nome-cognome');
 const kmViaggio = document.getElementById('km-viaggio');
 const etaUtente = document.getElementById('eta-utente');
 const btnGenera = document.getElementById('btn-genera');
 const btnAnnulla = document.getElementById('btn-annulla');
 
-// console.log(nomeCognome);
-// console.log(kmViaggio);
-// console.log(etaUtente);
-// console.log(btnGenera);
-// console.log(btnAnnulla);
-
+// valori per calcolo biglietto
 const pricePerKm = 0.21;
 const discountMinorenni = 20;
 const discountOver65 = 40;
 
+// click bottone genera
 btnGenera.addEventListener ('click',
   function() {
+    // valori input in assegnati a costanti
     const valNomeCognome = nomeCognome.value;
-    // console.log(valNomeCognome);
     const valKmViaggio = Number(kmViaggio.value);
-    // console.log(valKmViaggio);
     const valEtaUtente = etaUtente.value;
-    // console.log(valEtaUtente);
 
+    // comparsa biglietto sulla pagina web
+    document.getElementById('ticket-container').classList.add('appari');
+
+    // tipo di tariffa biglietto caso standard
+    document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(2) div').innerHTML = 'Tariffa Standard';
+    
+    // formula calcolo prezzo biglietto - casi under18 , over65 calcolo più tariffa biglietto
     let priceTicket = pricePerKm * valKmViaggio;
 
     if (valEtaUtente === 'Minorenne') {
       priceTicket *= (1 - discountMinorenni / 100);
+      document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(2) div').innerHTML = 'Tariffa Under18';
     } else if (valEtaUtente === 'Over65') {
       priceTicket *= (1 - discountOver65 / 100);
+      document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(2) div').innerHTML = 'Tariffa Over65';
     }
 
+    // prezzo finale su console
     console.log('Il prezzo del biglietto è:', priceTicket.toFixed(2));
+     
+    // campi del biglietto
+    document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(1) div').innerHTML = valNomeCognome;
+    document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(3) div').innerHTML = Math.floor(Math.random() * 10) + 1;
+    document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(4) div').innerHTML = Math.floor(Math.random() * 100000) + 1;
+    document.querySelector('#ticket .ticket-body .ticket-body-sect:nth-child(5) div').innerHTML = priceTicket.toFixed(2) + ' €';
+    
   }
 )
 
+// click bottone annulla
 btnAnnulla.addEventListener ('click',
   function() {
+    //scomparsa biglietto pagina web
+    document.getElementById('ticket-container').classList.remove('appari');
+
+    // reset valori degli input del calcolatore prezzo
     nomeCognome.value = '';
     kmViaggio.value = '';
     etaUtente.value = 'Minorenne';
